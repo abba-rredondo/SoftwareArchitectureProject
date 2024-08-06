e148a624b84e55960b333a7a53f8ec8466979374
abbarredondo <abarredondo@miuandes.cl>
2024-08-06 02:54:24 -0400

    Fixed docker and created models

---
+++ Webook/bookapp/models.py
@@ -0,0 +1,36 @@
+from django_cassandra_engine.models import DjangoCassandraModel
+from cassandra.cqlengine import columns
+
+
+class Author(DjangoCassandraModel):
+    id = columns.UUID(primary_key=True)
+    name = columns.Text(required=True, index=True)
+    date_of_birth = columns.Date()
+    country_of_origin = columns.Text()
+    description = columns.Text()
+
+class Book(DjangoCassandraModel):
+    id = columns.UUID(primary_key=True)
+    name = columns.Text(required=True, index=True)
+    summary = columns.Text()
+    date_of_publication = columns.Date()
+    number_of_sales = columns.BigInt(default=0)
+    author = columns.UUID()  # Referencia al ID del autor
+
+class Review(DjangoCassandraModel):
+    id = columns.UUID(primary_key=True)
+    book = columns.UUID()  # Referencia al ID del libro
+    review_text = columns.Text()
+    score = columns.Integer()
+    up_votes = columns.Integer(default=0)
+
+    class Meta:
+        get_pk_field = 'id'
+
+class Sales(DjangoCassandraModel):
+    book = columns.UUID(primary_key=True)
+    year = columns.Integer(primary_key=True)
+    sales = columns.BigInt(default=0)
+
+    class Meta:
+        get_pk_field = 'book' #book es la clave primaria de la tabla
