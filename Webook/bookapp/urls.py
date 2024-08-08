02ac363bf780050add192c638cf48de258c5cac2
macaferrari <mdferrari@miuandes.cl>
2024-08-08 00:12:42 -0400

    table of 10 best scores books done

--- Webook/bookapp/urls.py
+++ Webook/bookapp/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('books/create/', book_views.book_create, name='book_create'),
     path('books/update/<uuid:pk>/', book_views.book_update, name='book_update'),
     path('books/delete/<uuid:pk>/', book_views.book_delete, name='book_delete'),
+    path('books/top/', book_views.top_rated_books, name='top_rated_books'),
     path('reviews/', review_views.review_list, name='review_list'),
     path('reviews/create/', review_views.review_create, name='review_create'),
     path('reviews/<uuid:pk>/update/', review_views.review_update, name='review_update'),
--- Webook/bookapp/views/book_views.py
+++ Webook/bookapp/views/book_views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from ..models import Book, Author
+from ..models import Book, Author, Review, Sales
 from ..forms import BookForm
 from cassandra.cqlengine.query import DoesNotExist
 from django.contrib import messages
 from uuid import uuid4
+from cassandra.cluster import Cluster
+from django.db.models import Avg
 
 
 def book_list(request):
@@ -54,3 +56,52 @@ def book_delete(request, pk):
         book.delete()
         return redirect('book_list')
     return render(request, 'book_templates/book_confirm_delete.html', {'book': book})
+
+
+
+def top_rated_books(request):
+    # Paso 1: Calcular el puntaje promedio para cada libro
+    books = Book.objects.all()
+    book_scores = []
+
+    for book in books:
+        # Obtener reseñas del libro
+        reviews = Review.objects.filter(book=book.id)
+        total_score = 0
+        # Calcular el puntaje promedio
+        for review in reviews:
+            total_score += review.score
+        
+        review_count = reviews.count()
+        average_score = total_score / review_count if review_count > 0 else 0
+
+        book_scores.append({
+            'book': book,
+            'average_score': average_score
+        })
+    print('llegoooo aquiiiiiiiii')
+    top_books = sorted(book_scores, key=lambda x: x['average_score'], reverse=True)[:10]
+    print(top_books)
+    result = []
+
+    for item in top_books:
+        book = item['book']
+        average_score = item['average_score']
+
+
+        reviews = Review.objects.filter(book=book.id)
+        
+
+        highest_review = max(reviews, key=lambda r: r.score, default=None)
+        lowest_review = min(reviews, key=lambda r: r.score, default=None)
+        most_popular_review = max(reviews, key=lambda r: r.up_votes, default=None)
+
+        result.append({
+            'book': book,
+            'average_score': average_score,
+            'highest_review': highest_review,
+            'lowest_review': lowest_review,
+            'most_popular_review': most_popular_review
+        })
+
+    return render(request, 'book_templates/top_rated_books.html', {'books': result})
